2fa1ea11ebaaa55fd043302b7dbfe9ccfc247336
5064 <5og4.kirin@gmail.com>
2020-11-12 10:48:28 +0900

    wip

--- ArithmeticParser/src/main.ts
+++ ArithmeticParser/src/main.ts
@@ -15,7 +15,7 @@ class Source {
     }
 
     public next(): void {
-        ++this.pos;
+        ++this.pos
     }
 }
 
@@ -56,22 +56,22 @@ class Parser extends Source {
     }
 
     /**
-     * term = number, {("*", number) | ("/", number)}
+     * term = factor, {("*", factor) | ("/", factor)}
      * 
      * 項の計算
      * exprより優先度が高い乗算,除算をする
      */
     public term(): number {
-        let x: number = this.number()
-        while (true) {  // 構文解析と計算を分離せずにexprで処理しているのがポイント
+        let x: number = this.factor()
+        while (true) {
             switch (this.peek()) {
                 case "*":
                     this.next()
-                    x *= this.number()
+                    x *= this.factor()
                     continue
                 case "/":
                     this.next()
-                    x /= this.number()
+                    x /= this.factor()
                     continue
             }
             break
@@ -79,8 +79,47 @@ class Parser extends Source {
         return x
     }
 
+    /**
+     * factor = ("(", expr, ")") | number
+     * 
+     * 括弧を実装するための因子(factor)層
+     * 括弧の中はexprであるため、以下のように再帰が循環する
+     * expr -> term -> factor -> expr -> ...
+     */
+    public factor(): number {
+        if (this.peek() === '(') {
+            this.next()
+            const ret: number = this.expr()
+            if (this.peek() === ')') {
+                this.next()
+            }
+            return ret
+        }
+        return this.number()
+    }
+
+    /**
+     * 0除算
+     * javascript returns Infinity when 0 division
+     * But I wanna throw "ZeroDivisionError"
+     */
+    // public divisionFactor(): number {
+    //     if (this.peek() === '0') {
+    //         throw new ZeroDivisionError();
+    //     }
+    //     return this.factor()
+    // }
+
     private isDigit(char: string): boolean {
         return /^\d+$/.test(char)
+    }
+}
+
+export class ZeroDivisionError extends Error {
+    constructor(e?: string) {
+        super(e);
+        this.name = new.target.name;
+        Object.setPrototypeOf(this, new.target.prototype);
     }
 }
 
--- ArithmeticParser/test/main.ts
+++ ArithmeticParser/test/main.ts
@@ -1,9 +1,13 @@
 import { expect } from "chai";
 import { describe, it } from "mocha";
-import { Main } from "../src/main";
+import { Main, ZeroDivisionError } from "../src/main";
 
-describe("正常系", () => {
+describe("Normal usecases", () => {
     const testcases = [
+        {
+            expr: "666",
+            answer: 666
+        },
         {
             expr: "0+1+2+3",
             answer: 6
@@ -17,29 +21,54 @@ describe("正常系", () => {
             answer: -1
         },
         {
-            expr: "1-3-0",
-            answer: -2
-        },
-        {
             expr: "2*4*8",
             answer: 64
         },
         {
-            expr: "1+2*3",
-            answer: 7
+            expr: "10+20*30",
+            answer: 610
         },
         {
-            expr: "1*2+3",
-            answer: 5
+            expr: "10*20+30",
+            answer: 230
         },
         {
             expr: "2-4/8",
             answer: 1.5
         },
+        {
+            expr: "(4+5)*6",
+            answer: 54
+        },
+        {
+            expr: "4+(5*6)",
+            answer: 34
+        },
+        {
+            expr: "(2-4)*8/16",
+            answer: -1
+        },
+        {
+            expr: "2*(4/(6-8))",
+            answer: -4
+        },
     ]
     testcases.map((tc) => {
         it(`${tc.expr} = ${tc.answer}`, () => {
             expect(Main.parse(tc.expr)).to.equal(tc.answer);
+        })
+    })
+})
+
+describe("Exceptional usecases", () => {
+    describe("Zero division", () => {
+        const expr1 = "1/0";
+        it(`${expr1} throw ZeroDivisionError`, () => {
+            expect(() => Main.parse(expr1)).to.throw(ZeroDivisionError);
+        })
+        const expr2 = "2/3*(4-4)";
+        it(`${expr2} throw ZeroDivisionError`, () => {
+            expect(Main.parse(expr2)).to.throw(ZeroDivisionError);
         })
     })
 })
